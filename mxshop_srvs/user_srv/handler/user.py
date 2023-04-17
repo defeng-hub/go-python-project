50f402f0de85f5fa8a85d258b6d9332663fdf27c
wangdefeng <125554566@qq.com>
2023-04-17 15:51:03 +0800

    添加logger日志模块

--- mxshop_srvs/user_srv/handler/user.py
+++ mxshop_srvs/user_srv/handler/user.py
@@ -1,24 +1,26 @@
 import time
 
 import grpc
+from loguru import logger
+
 from user_srv.proto import user_pb2, user_pb2_grpc
 from user_srv.model.models import User
 
 
 class UserServicer(user_pb2_grpc.UserServicer):
+
+    @logger.catch
     def GetUserList(self, request: user_pb2.PageInfo, context):
         rsp = user_pb2.UserListResponse()
-
         users = User.select()
         rsp.Total = users.count()
         start = 0
-        per_page_nums = 10 # 每页数量
+        per_page_nums = 10  # 每页数量
         if request.PageSize:
             per_page_nums = request.PageSize
         if request.Page:
             start = per_page_nums * (request.Page - 1)
 
-        #
         users = users.offset(start).limit(per_page_nums)
 
         for user in users:
--- mxshop_srvs/user_srv/server.py
+++ mxshop_srvs/user_srv/server.py
@@ -2,15 +2,19 @@ from concurrent import futures
 import logging
 import grpc
 
+from loguru import logger
 from user_srv.proto import user_pb2, user_pb2_grpc
 from user_srv.handler.user import UserServicer
 
 
 def serve():
+    # 初始化logger
+    logger.add("logs/user_srv_{time}.log", rotation="125 MB")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_pb2_grpc.add_UserServicer_to_server(UserServicer(), server)
     server.add_insecure_port('[::]:50051')
-    print("启动服务0:50051")
+
+    logger.debug("启动服务0:50051")
     server.start()
     server.wait_for_termination()
 
